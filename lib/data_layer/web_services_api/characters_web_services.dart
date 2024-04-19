b8ea23bf785869ed669b72d697fe2bbf1bf8195c
ranamohs <ranamohsen868_sd@fcis.bsu.edu.eg>
2024-04-19 05:29:49 +0200

    characters webservices

--- lib/data_layer/web_services_api/characters_web_services.dart
+++ lib/data_layer/web_services_api/characters_web_services.dart
@@ -6,25 +6,24 @@ class CharactersWebServices{
  late Dio dio ;
  CharactersWebServices (){
    BaseOptions options = BaseOptions(
-
      baseUrl: baseUrl,
      receiveDataWhenStatusError: true,
-     connectTimeout: Duration(seconds: 20),
-     receiveTimeout: Duration(seconds: 20)
+     // connectTimeout: Duration(seconds: 20),
+     // receiveTimeout: Duration(seconds: 20)
    );
 
    dio = Dio(options);
  }
 
- Future<List<dynamic>> getAllCharacters() async{
+ Future<dynamic> getAllCharacters() async{
    try{
-     Response response = await dio.get('character');
+    final Response response = await dio.get('character');
      print(response.data.toString());
      return response.data;
    } catch(e){
      print(e.toString());
      return[];
    }
-   
+
  }
 }
