d76bfd4f3b96324db98949eb2ad1fbd2f0ae9eaa
ranamohs <ranamohsen868_sd@fcis.bsu.edu.eg>
2024-04-19 05:28:34 +0200

    characters drepository

--- lib/data_layer/repository/characters_repository.dart
+++ lib/data_layer/repository/characters_repository.dart
@@ -8,11 +8,45 @@ class CharactersRepository{
   CharactersRepository(this.charactersWebServices);
 
 
-  Future<List<Character>> getAllCharacters() async{
+
+ Future<Character> getAllCharacters() async{
 
     final characters = await charactersWebServices.getAllCharacters();
-    
-    return characters.map((character) => Character.fromJson(character)).toList( );
+    return  Character.fromJson(characters);
 
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// Future<List<Character>> getAllCharacters() async {
+//   final data = await charactersWebServices.getAllCharacters();
+//   //check if the returned data is not null
+//   if (data != null) {
+//     // Assuming the data is a Map with a key containing the List<dynamic>
+//     // For example: { "results": [ ... ] }
+//     //search for the first key in the map that contain a List-->data is a Map<k,v>, then store it in keyWithList variable
+//     //returns the first element that satisfies the function condition
+//     final keyWithList = data.keys.firstWhere(
+//           (key) => data[key] is List,
+//     );
+//     if (keyWithList != null) {
+//       List<dynamic> characterList = data[keyWithList];
+//       return characterList.map((characterJson) =>
+//           Character.fromJson(characterJson)).toList();
+//     } else {
+//       throw Exception('No List<dynamic> found in the API response');
+//     }
+//   } else {
+//     throw Exception('Failed to fetch data from API');
+//   }
+// }
