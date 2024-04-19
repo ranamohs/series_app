8fd3f342cbfcba93b309b2e8b277373f72b4b61f
ranamohs <ranamohsen868_sd@fcis.bsu.edu.eg>
2024-04-19 05:33:12 +0200

    constants

--- lib/constans/strings.dart
+++ lib/constans/strings.dart
@@ -1,3 +1,3 @@
-final baseUrl = 'https://rickandmortyapi.com/api';
+final baseUrl = 'https://rickandmortyapi.com/api/';
 const charactersScreen = '/';
 const characterDetailsScreen = '/character_details';
