586d6e7a8175f6b182a7318d9aeaed77f867e2db
ranamohs <ranamohsen868_sd@fcis.bsu.edu.eg>
2024-04-19 05:30:03 +0200

    app colors

--- lib/constans/colors.dart
+++ lib/constans/colors.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors{
-  static const Color primaryColor =Color(0xffB39DDB);
-  static const Color secondryColor =Color(0xff343479);
+  static const Color primaryColor =Color(0xff3596B5);
+  static const Color secondryColor =Color(0xff296073);
   static const Color whiteColor =Color(0xffE1E8EB);
+  static const Color dividerColor =Color(0xffE1E8EB);
 
 }
